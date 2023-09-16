91ffae79235f2c25cf1254c474787c81a1e20212
PieroCruz27 <pierobpc7@gmail.com>
2023-09-15 23:55:13 -0500

    inicio-piero

--- src/main/java/net/prestamo/Controller/ServletLoginUsuarios.java
+++ src/main/java/net/prestamo/Controller/ServletLoginUsuarios.java
@@ -21,7 +21,7 @@ public class ServletLoginUsuarios extends HttpServlet {
        
     }
 
-
+    //ser
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		MYQLLoginUsuarioDAO dao = new MYQLLoginUsuarioDAO();
 		Usuarios u = new Usuarios();
